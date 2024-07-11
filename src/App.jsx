8063da6c3a12a0d8f99b1bac1fb3ca6d3e837dfd
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-11 13:13:51 -0700

    Add a button to toggle between dark and light themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
 
-  return <></>;
-}
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <div className={darkTheme ? "dark" : "light"}>
+      {/* TODO: implement dark-light effect*/}
+      {/* TODO: reference header size */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px 20px",
+          borderBottom: "1px solid #ddd",
+        }}
+      >
+        {/* TODO: reference typographies*/}
+        <div>ImageUpload</div>
+        <button
+          onClick={toggleTheme}
+          style={{
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          {/* TODO: reference real icon */}
+          {darkTheme ? "🌙" : "☀️"}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
